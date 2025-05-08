0d4c780fecdf093ca0ef6e4110826c1874a544a0
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2025-05-08 13:30:27 -0600

    feat: create lib shared

---
+++ libs/adrian-badilla/frontend/shared/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/lib.routes';
+
+export * from './lib/adrian-badilla-ui-shared/adrian-badilla-ui-shared.component';
---
+++ libs/adrian-badilla/frontend/shared/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { AdrianBadillaUiSharedComponent } from './adrian-badilla-ui-shared/adrian-badilla-ui-shared.component';
+
+export const adrianBadillaUiSharedRoutes: Route[] = [
+  { path: '', component: AdrianBadillaUiSharedComponent },
+];
