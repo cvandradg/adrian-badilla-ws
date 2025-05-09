93133884cbe79684da06324fb8e507b8b6919414
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2025-05-09 16:56:54 -0600

    feat: create components

--- libs/adrian-badilla/frontend/landing-page/src/lib/adrian-badilla-ui-landing-page/adrian-badilla-ui-landing-page.component.ts
+++ libs/adrian-badilla/frontend/landing-page/src/lib/adrian-badilla-ui-landing-page/adrian-badilla-ui-landing-page.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroComponent } from '../components/hero/hero.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'adrian-badilla-landing-page',
-  imports: [CommonModule],
+  imports: [CommonModule, HeroComponent, NavbarComponent],
   templateUrl: './adrian-badilla-ui-landing-page.component.html',
   styleUrl: './adrian-badilla-ui-landing-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
