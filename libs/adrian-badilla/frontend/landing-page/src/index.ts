2082c5adbbee106a0e7ddb841ddecdc41f291524
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2025-05-20 14:02:08 -0600

    feat: create cards component

--- libs/adrian-badilla/frontend/landing-page/src/index.ts
+++ libs/adrian-badilla/frontend/landing-page/src/index.ts
@@ -7,3 +7,5 @@ export * from './lib/components/hero/hero.component';
 export * from './lib/components/navbar/navbar.component';
 
 export * from './lib/components/footer/footer.component';
+
+export * from './lib/components/cards/cards.component';
--- libs/adrian-badilla/frontend/landing-page/src/lib/adrian-badilla-ui-landing-page/adrian-badilla-ui-landing-page.component.ts
+++ libs/adrian-badilla/frontend/landing-page/src/lib/adrian-badilla-ui-landing-page/adrian-badilla-ui-landing-page.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeroComponent } from '../components/hero/hero.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { CardsComponent } from '../components/cards/cards.component';
 
 @Component({
   standalone: true,
   selector: 'adrian-badilla-landing-page',
-  imports: [CommonModule, HeroComponent, NavbarComponent, FooterComponent],
+  imports: [CommonModule, HeroComponent, NavbarComponent, FooterComponent, CardsComponent],
   templateUrl: './adrian-badilla-ui-landing-page.component.html',
   styleUrl: './adrian-badilla-ui-landing-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
