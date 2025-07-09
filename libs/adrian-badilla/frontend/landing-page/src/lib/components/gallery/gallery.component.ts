431eddd51cc1c7a5e3c341fa668b1889d7364010
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2025-07-09 11:38:24 -0600

    feat: Design and logic for gallery segment

--- libs/adrian-badilla/frontend/landing-page/src/lib/components/gallery/gallery.component.ts
+++ libs/adrian-badilla/frontend/landing-page/src/lib/components/gallery/gallery.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+// gallery.component.ts
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'adrian-badilla-gallery',
@@ -6,7 +15,41 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule],
 })
-export class GalleryComponent {}
+export class GalleryComponent implements AfterViewInit {
+  carousels = [
+    {
+      images: this.generatePaths('/global/assets/gallery/img-', 10),
+      reverse: false,
+      ref: 'track1',
+    },
+    {
+      images: this.generatePaths('/global/assets/gallery2/img-', 7),
+      reverse: true,
+      ref: 'track2',
+    },
+    {
+      images: this.generatePaths('/global/assets/gallery3/img-', 9),
+      reverse: false,
+      ref: 'track3',
+    },
+  ];
 
+  @ViewChildren('carouselTrack') trackRefs!: QueryList<ElementRef<HTMLDivElement>>;
 
+  ngAfterViewInit() {
+    this.trackRefs.forEach((trackRef) => {
+      const track = trackRef.nativeElement;
+      const totalWidth = track.scrollWidth / 2;
+      const duration = totalWidth / 100;
+
+      track.style.setProperty('--scroll-width', `-${totalWidth}px`);
+      track.style.setProperty('--scroll-duration', `${duration}s`);
+    });
+  }
+
+  private generatePaths(basePath: string, count: number): string[] {
+    return Array.from({ length: count }, (_, i) => `${basePath}${i + 1}.jpg`);
+  }
+}
