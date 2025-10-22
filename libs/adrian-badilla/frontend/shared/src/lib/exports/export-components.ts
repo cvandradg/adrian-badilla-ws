4bddc3af2942be7d35049fb10b51537dd54289f9
FabianRG1990 <fabian.soy1990@gmail.com>
2025-10-21 23:06:22 -0600

    feat(ui): implement primary animated button

--- libs/adrian-badilla/frontend/shared/src/lib/exports/export-components.ts
+++ libs/adrian-badilla/frontend/shared/src/lib/exports/export-components.ts
@@ -1,15 +1,15 @@
 // import { StrengthMeterComponent } from '../components/strength-meter/strength-meter.component';
-// import { PrimaryAnimatedButtonComponent } from '../components/primary-animated-button/primary-animated-button.component';
 // import { SecondaryAnimatedButtonComponent } from '../components/secondary-animated-button/secondary-animated-button.component';
 // import { TertiaryAnimatedButtonComponent } from '../components/tertiary-animated-button/tertiary-animated-button.component';
 // import { NavbarComponent } from '../components/navbar/navbar.component';
 // import { NoContentMessageComponent } from './../components/no-content-message/NoContentMessage.component';
+import { PrimaryAnimatedButtonComponent } from '../components/primary-animated-button/primary-animated-button.component';
 import { StatusMessageComponent } from '../components/status-message/status-message.component';
 
 export const COMPONENTS = [
   StatusMessageComponent,
   // StrengthMeterComponent,
-  // PrimaryAnimatedButtonComponent,
+  PrimaryAnimatedButtonComponent,
   // SecondaryAnimatedButtonComponent,
   // TertiaryAnimatedButtonComponent,
   // StrengthMeterComponent,
@@ -19,7 +19,7 @@ export const COMPONENTS = [
 
 export * from '../components/status-message/status-message.component';
 // export * from '../components/strength-meter/strength-meter.component';
-// export * from '../components/primary-animated-button/primary-animated-button.component';
+export * from '../components/primary-animated-button/primary-animated-button.component';
 // export * from '../components/tertiary-animated-button/tertiary-animated-button.component';
 // export * from '../components/secondary-animated-button/secondary-animated-button.component';
 // export * from '../components/no-content-message/NoContentMessage.component';
