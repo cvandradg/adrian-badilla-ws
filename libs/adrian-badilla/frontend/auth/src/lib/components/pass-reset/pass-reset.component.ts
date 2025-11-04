4671b1a959eb128975997a073ea4d2f2b891c52f
FabianRG1990 <fabian.soy1990@gmail.com>
2025-11-03 21:43:54 -0600

    feat: import  pass-reset.component from the old application

--- libs/adrian-badilla/frontend/auth/src/lib/components/pass-reset/pass-reset.component.ts
+++ libs/adrian-badilla/frontend/auth/src/lib/components/pass-reset/pass-reset.component.ts
@@ -1,21 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { passResetStore } from './pass-reset.store';
-import { Component, inject, Input } from '@angular/core';
-import { BaseComponent } from '@adrianbadilla/shared/classes/base-component';
-import { MODULES } from '@adrianbadilla/shared/exports/export-modules';
-import { COMPONENTS } from '@adrianbadilla/shared/exports/export-components';
+import { Component, Input } from '@angular/core';
+import { COMPONENTS, MODULES } from '@adrian-badilla/ui/shared';
 
 @Component({
-  selector: 'adrianbadilla-pass-reset',
+  selector: 'adrian-badilla-pass-reset',
   templateUrl: './pass-reset.component.html',
   styleUrls: ['./pass-reset.component.scss'],
   standalone: true,
   imports: [CommonModule, MODULES, COMPONENTS],
-  providers: [passResetStore],
 })
-export class PassResetComponent extends BaseComponent {
+export class PassResetComponent {
   @Input()
   public user!: string;
 
-  passResetStore = inject(passResetStore);
 }
