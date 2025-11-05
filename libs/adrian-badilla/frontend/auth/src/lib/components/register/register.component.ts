5a55feb21f10cffdd20e3a16944e12e0df39bdd3
FabianRG1990 <fabian.soy1990@gmail.com>
2025-11-04 23:30:09 -0600

    feat: createAccount test

--- libs/adrian-badilla/frontend/auth/src/lib/components/register/register.component.ts
+++ libs/adrian-badilla/frontend/auth/src/lib/components/register/register.component.ts
@@ -1,9 +1,13 @@
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Subject } from 'rxjs';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject } from '@angular/core';
 import { COMPONENTS, MODULES, validations } from '@adrian-badilla/ui/shared';
 import { FormBuilder, Validators } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { firebaseAuthStore } from '../../data-access/auth.store';
+
+
 
 @Component({
   selector: 'adrian-badilla-register',
@@ -13,6 +17,9 @@ import { FormBuilder, Validators } from '@angular/forms';
   imports: [CommonModule, RouterModule, COMPONENTS, MODULES],
 })
 export class RegisterComponent {
+firebaseAuthStore = inject(firebaseAuthStore);
+
+
   isPassStrong$ = new Subject<boolean>();
 
   formBuilder = inject(FormBuilder);
@@ -22,13 +29,31 @@ export class RegisterComponent {
     pass: validations(),
   });
 
-  prueba(){
-    const user = this.loginInputForm.get('user')?.value;
-    const pass = this.loginInputForm.get('pass')?.value;
+  formChangesSignal = toSignal(this.loginInputForm.valueChanges, {
+    initialValue: this.loginInputForm.value,
+  });
 
-    console.log('Correo:', user);
-    console.log('Contraseña:', pass);
-    console.log('prueba del click')}
+constructor() {
+  effect(() => {
+    console.log('form changed:', this.formChangesSignal());
+  });
+}
+
+  get credentials() {
+    return {
+      user: this.loginInputForm.get('user')?.value as string,
+      pass: this.loginInputForm.get('pass')?.value as string,
+    };
+  }
+
+
+prueba() {
+  this.firebaseAuthStore.createAccount(this.credentials);
+
+  console.log('Datos enviados para crear cuenta:', this.credentials);
+}
+
+  }
 
   // isValidUser$ = this.loginInputForm.valueChanges.pipe(
   //   map(() => !this.loginInputForm.controls.user.invalid)
@@ -37,4 +62,12 @@ export class RegisterComponent {
   // enableButton$ = combineLatest([this.isValidUser$, this.isPassStrong$]).pipe(
   //   map(([isValidUser, isPassStrong]) => isValidUser && isPassStrong)
   // );
-}
+
+// function toSignal(
+//   valueChanges: Observable<
+//     Partial<{ user: string | null; pass: string | null }>
+//   >,
+//   arg1: { initialValue: Partial<{ user: string | null; pass: string | null }> }
+// ) {
+//   throw new Error('Function not implemented.');
+// }
--- libs/adrian-badilla/frontend/auth/src/lib/data-access/auth.store.ts
+++ libs/adrian-badilla/frontend/auth/src/lib/data-access/auth.store.ts
@@ -1,7 +1,7 @@
 import { signalStore, withMethods, withProps } from '@ngrx/signals';
 import { withDevtools } from '@angular-architects/ngrx-toolkit';
 import { inject } from '@angular/core';
-import { FirebaseAuthService } from '@adrian-badilla/ui/shared';
+import { Credentials, FirebaseAuthService } from '@adrian-badilla/ui/shared';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { exhaustMap, pipe, tap } from 'rxjs';
 
@@ -15,7 +15,16 @@ export const firebaseAuthStore = signalStore(
     googleSignIn: rxMethod<void>(
       pipe(
         exhaustMap(() => store.firebaseAuthService.googleSignin()),
-        tap((respuesta) => console.log('Respuesta de Google Sign-In:', respuesta))
+        tap((respuesta) =>
+          console.log('Respuesta de Google Sign-In:', respuesta)
+        )
+      )
+    ),
+
+    createAccount: rxMethod<Credentials>(
+      pipe(
+        exhaustMap((creds) => store.firebaseAuthService.createAccount(creds)),
+        tap((resp) => console.log('Cuenta creada Firebase:', resp))
       )
     ),
   }))
