689f7454f5fde261641d7f2f2783507e7f26a4b1
FabianRG1990 <fabian.soy1990@gmail.com>
2025-11-11 18:53:29 -0600

    Step two: verify that the button works using console.log

--- libs/adrian-badilla/frontend/auth/src/lib/components/pass-reset/pass-reset.component.ts
+++ libs/adrian-badilla/frontend/auth/src/lib/components/pass-reset/pass-reset.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { COMPONENTS, MODULES } from '@adrian-badilla/ui/shared';
 
 @Component({
+  standalone: true,
   selector: 'adrian-badilla-pass-reset',
   templateUrl: './pass-reset.component.html',
   styleUrls: ['./pass-reset.component.scss'],
@@ -11,4 +12,7 @@ export class PassResetComponent {
   @Input()
   public user!: string;
 
+  onResetClick() {
+    console.log('Botón de recuperar contraseña funcionando');
+  }
 }
