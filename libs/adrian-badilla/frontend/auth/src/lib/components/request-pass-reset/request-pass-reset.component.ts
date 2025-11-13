21b54f870028756bb4cd40500ec1e698319270e6
FabianRG1990 <fabian.soy1990@gmail.com>
2025-11-13 12:58:07 -0600

    3: read and verify the oobCode from the URL
    Load the component from a real URL with ?oobCode=XYZ.
    
    Get the oobCode inside the component.
    
    Print it with a console.log to ensure it arrives.
    
    Check three possible cases:
    
    "oobCode has a value" → correct
    
    "oobCode is empty string" → check URL
    
    "oobCode is null" → check route or Firebase
    
    This verification ensures that the component can function when the user comes from the real email.

--- libs/adrian-badilla/frontend/auth/src/lib/components/request-pass-reset/request-pass-reset.component.ts
+++ libs/adrian-badilla/frontend/auth/src/lib/components/request-pass-reset/request-pass-reset.component.ts
@@ -1,8 +1,7 @@
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Subject } from 'rxjs';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { COMPONENTS, MODULES } from '@adrian-badilla/ui/shared';
-
 
 @Component({
   selector: 'adrian-badilla-request-pass-reset',
@@ -12,10 +11,25 @@ import { COMPONENTS, MODULES } from '@adrian-badilla/ui/shared';
   imports: [RouterModule, MODULES, COMPONENTS],
 })
 export class RequestPassResetComponent {
+  route = inject(ActivatedRoute);
 
   isPassStrong$ = new Subject<boolean>();
 
-  log(){
+  log() {
     console.log('El boton funciona');
+
+    const oobCode = this.route.snapshot.queryParamMap.get('oobCode');
+
+    if (oobCode) {
+      console.log('✔️ oobCode válido:', oobCode);
+    } else if (oobCode === '') {
+      console.warn('⚠️ oobCode llegó vacío. Revisar URL del correo.');
+    } else if (oobCode === null) {
+      console.error(
+        '❌ No existe oobCode en la URL. Usuario entró manualmente o Firebase no lo envió.'
+      );
+    }
+
+    console.log('oobCode recibido:', oobCode);
   }
 }
