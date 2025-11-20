3a32c2224d50cac2b75891620f1b0fb44ee5bf91
FabianRG1990 <fabian.soy1990@gmail.com>
2025-11-19 19:51:43 -0600

    2: Make a route to see what's being rendered in the browser

--- libs/adrian-badilla/frontend/auth/src/lib/lib.routes.ts
+++ libs/adrian-badilla/frontend/auth/src/lib/lib.routes.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { RequestPassResetComponent } from './components/request-pass-reset/request-pass-reset.component';
+import { EmailVerificationComponent } from './components/email-verification/email-verification.component';
 
 export default [
   { path: 'login', component: LoginComponent },
@@ -14,6 +15,11 @@ export default [
   {
     path: 'request-pass-reset',
     component: RequestPassResetComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'email-verification',
+    component: EmailVerificationComponent,
     pathMatch: 'full',
   },
 ] as Route[];
