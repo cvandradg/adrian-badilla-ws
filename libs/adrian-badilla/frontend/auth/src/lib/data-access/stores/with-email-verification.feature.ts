f83633169b12f812fafe9d656580c1209ca436f6
FabianRG1990 <fabian.soy1990@gmail.com>
2025-12-29 18:28:49 -0600

    paso 3.1
    3.1 – Crear un rxMethod con un observable falso from('hola')
    
    Ahora hacemos lo mismo, pero ya con rxMethod:
    
    En el Feature de Email Verification (por ejemplo withEmailVerificationResources):
    
    Crear un método con rxMethod<void> que:
    
    Internamente haga from(['hola']).pipe(tap(x => console.log('probando log', x))).
    
    Probar en el navegador:
    
    Ver que aparece en consola: probando log hola.
    
    Esto comprueba que:
    
    El rxMethod se ejecuta correctamente.
    
    La interconexión componente → store → rxMethod → consola está funcionando.

--- libs/adrian-badilla/frontend/auth/src/lib/data-access/stores/with-email-verification.feature.ts
+++ libs/adrian-badilla/frontend/auth/src/lib/data-access/stores/with-email-verification.feature.ts
@@ -2,6 +2,8 @@ import { signalStoreFeature, withMethods, withProps } from '@ngrx/signals';
 import { withCustomCallState } from './with-custom-call-state.feature';
 import { FirebaseAuthService } from '@adrian-badilla/ui/shared';
 import { inject } from '@angular/core';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { from, tap } from 'rxjs';
 
 export function withEmailVerificationResources() {
   return signalStoreFeature(
@@ -11,9 +13,11 @@ export function withEmailVerificationResources() {
     })),
 
     withMethods(() => ({
-      testLog() {
-        console.log('Signal Feature Store funcionando! 🎉', {});
-      },
+  testRx: rxMethod<void>(() =>
+    from(['hola']).pipe(
+      tap(x => console.log('probando log', x))
+    )
+  ),
     }))
   );
 }
