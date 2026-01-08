1126b1925efe6b67a33f0447331fc864ae06567e
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2026-01-08 15:25:56 -0600

    test

--- libs/adrian-badilla/frontend/auth/src/lib/components/email-verification/email-verification.component.spec.ts
+++ libs/adrian-badilla/frontend/auth/src/lib/components/email-verification/email-verification.component.spec.ts
@@ -1,7 +1,6 @@
 import {
   MODULES,
   COMPONENTS,
-  FirebaseAuthService,
 } from '@adrian-badilla/ui/shared';
 import { EmailVerificationComponent } from './email-verification.component';
 import { inject, provideAppInitializer } from '@angular/core';
@@ -9,7 +8,6 @@ import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { firebaseAuthStore } from '../../data-access/stores/auth.store';
 import { FontAwesomeicons } from '../../../../../shared/src/lib/assets/icons/fontawesome';
-import { RouterTestingModule } from '@angular/router/testing';
 
 jest.mock('firebase/auth', () => ({
   GoogleAuthProvider: {},
@@ -27,11 +25,9 @@ describe('EmailVerificationComponent', () => {
         EmailVerificationComponent,
         COMPONENTS,
         MODULES,
-        RouterTestingModule,
       ],
       providers: [
         { provide: firebaseAuthStore, useValue: mockStore },
-        { provide: FirebaseAuthService, useValue: {} },
         provideAppInitializer(() => {
           inject(FaIconLibrary).addIcons(...FontAwesomeicons);
         }),
@@ -40,7 +36,6 @@ describe('EmailVerificationComponent', () => {
 
     fixture = TestBed.createComponent(EmailVerificationComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
