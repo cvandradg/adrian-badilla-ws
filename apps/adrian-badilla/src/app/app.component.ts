1d581f8c8e9717657ee6d38ea9d10f3d02b994bf
Vicious <137929047+Darkmixt12@users.noreply.github.com>
2026-05-07 01:57:31 -0600

    IA Chat component

--- apps/adrian-badilla/src/app/app.component.ts
+++ apps/adrian-badilla/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NutritionChatComponent } from 'adrian-badilla/settings';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule,NutritionChatComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
